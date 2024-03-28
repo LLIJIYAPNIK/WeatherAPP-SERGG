fb75ca21918936e702ce393c5365cf2a4b208bce
LLIJIYAPNIK <kas4wot@yandex.ru>
2024-03-28 21:31:34 +0300

    Добавлены стоковые иконки для погоды. Возобновлена работа с API

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import sys
 from PyQt5 import QtCore, uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QComboBox, QLabel, QCompleter, QVBoxLayout
 import sqlite3
 import requests
+import json
 
 connection = sqlite3.connect('cities.db')
 cur = connection.cursor()
 
-# access_key = '251f5b1c-ba22-4437-a2fe-86a9428986fe'
-#
-# headers = {
-#     'X-Yandex-Weather-Key': access_key
-# }
-#
-# response = requests.get('https://api.weather.yandex.ru/v2/forecast?lat=52.37125&lon=4.89388', headers=headers)
-#
-# print(response.json())
+
+access_key = '251f5b1c-ba22-4437-a2fe-86a9428986fe'
+
+headers = {
+    'X-Yandex-Weather-Key': access_key
+}
+
+response = requests.get('https://api.weather.yandex.ru/v2/forecast?lat=55.7522200&lon=37.6155600', headers=headers)
+
+# json_data = json.dumps(response.json(), indent=4)
+
+with open('test_1.json', 'w') as f:
+    json.dump(response.json(), f, ensure_ascii=False, indent=4)
 
 
 class MainWindow(QMainWindow):
@@ -35,26 +40,66 @@ class MainWindow(QMainWindow):
 
         self.initUI()
 
-
     def initUI(self):
         self.homeb.clicked.connect(self.home_page_f)
         self.searchb.clicked.connect(self.search_page_f)
         self.addb.clicked.connect(self.add_page_f)
         self.profileb.clicked.connect(self.profile_page_f)
 
-
     def home_page_f(self):
         self.stackedWidget.setCurrentIndex(0)
+        self.homeb.setStyleSheet("background-color: rgba(255, 255, 255, 100);\nborder: none;\nborder-radius: 15px;")
+        self.searchb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+        self.addb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+        self.profileb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+
 
     def search_page_f(self):
         self.stackedWidget.setCurrentIndex(1)
+        self.homeb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+        self.searchb.setStyleSheet("background-color: rgba(255, 255, 255, 100);\nborder: none;\nborder-radius: 15px;")
+        self.addb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+        self.profileb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+
+        self.layout = QVBoxLayout()
+
+        cities = [x[0] for x in list(cur.execute("SELECT Город FROM city").fetchall())]
+        print(cities)
+        completer = QCompleter(cities)
+        completer.popup().setStyleSheet("""
+                    QListView {
+                        background-color: #f0f0f0;
+                        border: 2px solid #333;
+                        border-radius: 5px;
+                    }
+                    QListView:item {
+                        padding: 5px;
+                        background-color: #000;
+                    }
+                    QListView:item:selected {
+                        background-color: #00f;
+                        color: #fff;
+                    }
+                """)
+
+        self.search_le.setCompleter(completer)
+        self.layout.addWidget(self.search_le)
+
+        self.setLayout(self.layout)
 
     def add_page_f(self):
         self.stackedWidget.setCurrentIndex(2)
+        self.homeb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+        self.searchb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+        self.addb.setStyleSheet("background-color: rgba(255, 255, 255, 100);\nborder: none;\nborder-radius: 15px;")
+        self.profileb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
 
     def profile_page_f(self):
         self.stackedWidget.setCurrentIndex(3)
-
+        self.homeb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+        self.searchb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+        self.addb.setStyleSheet("background-color: rgba(255, 255, 255, 0);\nborder: none;\nborder-radius: 15px;")
+        self.profileb.setStyleSheet("background-color: rgba(255, 255, 255, 100);\nborder: none;\nborder-radius: 15px;")
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -101,7 +146,8 @@ class MainWindow(QMainWindow):
                 if self.mouse_pressed and self.inside_area_future or (x1 <= event.x() <= x2 and y1 <= event.y() <= y2):
                     self.future.horizontalScrollBar().setValue(new_value_future)
 
-                if self.mouse_pressed and self.inside_area_hours or (x1_ <= event.x() <= x2_ and y1_ <= event.y() <= y2_):
+                if self.mouse_pressed and self.inside_area_hours or (
+                        x1_ <= event.x() <= x2_ and y1_ <= event.y() <= y2_):
                     self.hours.horizontalScrollBar().setValue(new_value_hours)
 
                 self.last_pos = event.pos()
